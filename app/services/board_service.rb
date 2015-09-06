30523efb629629f856fa0a1ec202cd405fd035eb
Tobi Lehman <tobi.lehman@gmail.com>
2015-09-06 13:26:42 -0700

    Prevent non-players from playing

--- app/services/board_service.rb
+++ app/services/board_service.rb
@@ -1,5 +1,6 @@
 module BoardService
   def self.attempt_move(x:, y:, user:, board:)
+    return "user has no permission: not in match" if !in_match?(user, board.match)
     return "color is nil" if color(user, board).nil?
     return "coordinates invalid (cannot be nil)" if x.nil? || y.nil?
     return "point (#{x},#{y}) is out of bounds" if out_of_bounds?(x,y,board.size)
@@ -10,6 +11,10 @@ module BoardService
   def self.out_of_bounds?(x,y,size)
     return false if x.floor != x || y.floor != y
     (x < 1 || y < 1) || (x > size || y > size)
+  end
+
+  def self.in_match?(user, match)
+    user.id.in?([match.black_user_id, match.white_user_id])
   end
 
   def self.color(user, board)
